a25a0419907aec095a5e5ebd1947cc1956a07eb8
Brian Woo <bwoo@bwooAndroid.(none)>
2015-07-24 21:47:24 -0600

    first commit

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bwoo/Downloads/coursera-android-github/django-tutorial/ENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
